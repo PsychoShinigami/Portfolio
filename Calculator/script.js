c3024cf72c9c1829fafd1b62cbbbe4886daafd3d
PsychoShinigami <ayushrajkeshri95@gmail.com>
2026-05-23 12:57:13 +0530

    .

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,22 +1,23 @@
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('button');
 
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
+function calculate(btn) {
+  btn.addEventListener('click', () => {
+    const value = btn.innerText;
+    if (value === '=') {
+      try {
+        display.innerText = eval(display.innerText);
+      } catch (e) {
+        display.innerText = 'Error';
+      }
+    } else if (value === 'AC') {
+      display.innerText = '';
+    } else if (value === 'C') {
+      display.innerText = display.innerText.slice(0, -1);
+    } else {
+      display.innerText += value;
+    }
+  });
+}
 
-        const value = btn.innerText;
-        if (value === '=') {
-            try {
-                display.innerText = eval(display.innerText);
-            } catch (e) {
-                display.innerText = "Error";
-            }
-        } else if (value === 'AC') {
-            display.innerText = "";
-        } else if (value === 'C') {
-            display.innerText = display.innerText.slice(0, -1);
-        } else {
-            display.innerText += value;
-        }
-    });
-});
+buttons.forEach(calculate);
